668193c9b74e73c6d4d13ccab293c4e098aaa3b7
shalindasilva1 <shalindasilva1@gmail.com>
2019-05-09 01:20:44 +0530

    http done

--- PropertyUpdateApi/Startup.cs
+++ PropertyUpdateApi/Startup.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using PropertyUpdateApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+
+
 
 namespace PropertyUpdateApi
 {
@@ -30,15 +27,24 @@ namespace PropertyUpdateApi
 
             services.AddDbContext<PropertyUpdateApiContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("PropertyUpdateApiContext")));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "PropertyAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Property Update V1");
+            });
 
             app.UseMvc();
         }
